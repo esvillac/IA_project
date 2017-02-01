fcedf622d46c918faf1e1b936f007ea44b955c16
esvillac <esvillac@espol.edu.ec>
2017-02-01 00:46:28 -0500

    pantalla de experto que permitira editar datos de las materias

--- src/ia/PantallaPrincipal.java
+++ src/ia/PantallaPrincipal.java
@@ -33,6 +33,7 @@ public class PantallaPrincipal extends javax.swing.JFrame {
     private void initComponents() {
 
         jComboBox1 = new javax.swing.JComboBox<>();
+        buttonGroup1 = new javax.swing.ButtonGroup();
         jPanel2 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
@@ -42,6 +43,11 @@ public class PantallaPrincipal extends javax.swing.JFrame {
         jTextProm = new javax.swing.JTextField();
         jTextNombreEst = new javax.swing.JTextField();
         jLabel5 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        f_password = new javax.swing.JPasswordField();
+        jPanel1 = new javax.swing.JPanel();
+        rb_experto = new javax.swing.JRadioButton();
+        rb_estudiante = new javax.swing.JRadioButton();
 
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
@@ -88,31 +94,90 @@ public class PantallaPrincipal extends javax.swing.JFrame {
         jLabel5.setFont(new java.awt.Font("FreeSerif", 1, 16)); // NOI18N
         jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ia/IA_LOGO.png"))); // NOI18N
 
+        jLabel4.setFont(new java.awt.Font("FreeSerif", 1, 16)); // NOI18N
+        jLabel4.setText("Contraseña");
+
+        f_password.setText("jPasswordField1");
+
+        jPanel1.setBackground(new java.awt.Color(220, 160, 99));
+
+        rb_experto.setBackground(new java.awt.Color(220, 160, 99));
+        buttonGroup1.add(rb_experto);
+        rb_experto.setText("Experto");
+        rb_experto.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                rb_expertoActionPerformed(evt);
+            }
+        });
+
+        rb_estudiante.setBackground(new java.awt.Color(220, 160, 99));
+        buttonGroup1.add(rb_estudiante);
+        rb_estudiante.setText("Estudiante");
+        rb_estudiante.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                rb_estudianteActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(rb_estudiante)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(rb_experto)
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(rb_experto)
+                    .addComponent(rb_estudiante))
+                .addContainerGap())
+        );
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(0, 23, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addGap(0, 24, Short.MAX_VALUE)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel2)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel3))
-                .addGap(124, 124, 124)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel3))
+                        .addGap(124, 124, 124))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addGap(18, 18, 18)))
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jTextNivel, javax.swing.GroupLayout.PREFERRED_SIZE, 212, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jTextNombreEst, javax.swing.GroupLayout.PREFERRED_SIZE, 212, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jTextProm, javax.swing.GroupLayout.PREFERRED_SIZE, 212, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(91, 91, 91))
             .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel5))
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(226, 226, 226)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel5))
+                        .addGap(81, 81, 81)
+                        .addComponent(f_password, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(109, 109, 109))))
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(162, 162, 162)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -130,22 +195,26 @@ public class PantallaPrincipal extends javax.swing.JFrame {
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(jTextProm, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 66, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(f_password, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(22, 22, 22)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(46, 46, 46))
+                .addGap(22, 22, 22))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -153,7 +222,7 @@ public class PantallaPrincipal extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        if(!(jTextNivel.getText().isEmpty()) && !(jTextNombreEst.getText().isEmpty()) && !(jTextProm.getText().isEmpty())){
+        if(!(jTextNivel.getText().isEmpty()) && !(jTextNombreEst.getText().isEmpty()) && !(jTextProm.getText().isEmpty()) && (rb_estudiante.isSelected()==true)){
             PantallaActExtracurricular ventanaAcademica = new PantallaActExtracurricular();
             ventanaAcademica.setVisible(true);
             this.setVisible(false);
@@ -161,6 +230,13 @@ public class PantallaPrincipal extends javax.swing.JFrame {
         else{
             System.out.println("LLene todos los campos");
         }
+         
+    if(rb_experto.isSelected()==true)
+
+    {       PantallaExperto ventanaExperto = new PantallaExperto();
+        ventanaExperto.setVisible(true);
+        this.setVisible(false);
+    }
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jTextNivelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextNivelActionPerformed
@@ -174,6 +250,16 @@ public class PantallaPrincipal extends javax.swing.JFrame {
     private void jTextNombreEstActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextNombreEstActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextNombreEstActionPerformed
+
+    private void rb_expertoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rb_expertoActionPerformed
+        f_password.setEnabled(true);
+        jLabel4.setEnabled(true);
+    }//GEN-LAST:event_rb_expertoActionPerformed
+
+    private void rb_estudianteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rb_estudianteActionPerformed
+        f_password.setEnabled(false);
+        jLabel4.setEnabled(false);
+    }//GEN-LAST:event_rb_estudianteActionPerformed
 
     /**
      * @param args the command line arguments
@@ -212,15 +298,21 @@ public class PantallaPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JPasswordField f_password;
     private javax.swing.JButton jButton1;
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     public static javax.swing.JTextField jTextNivel;
     public static javax.swing.JTextField jTextNombreEst;
     public static javax.swing.JTextField jTextProm;
+    private javax.swing.JRadioButton rb_estudiante;
+    private javax.swing.JRadioButton rb_experto;
     // End of variables declaration//GEN-END:variables
 }
--- src/ia/PruebaConexion.java
+++ src/ia/PruebaConexion.java
@@ -19,7 +19,7 @@ public class PruebaConexion {
         try {
             Class.forName("org.postgresql.Driver");
             //Establecemos el Usuario y la contraseña
-            conexion = DriverManager.getConnection(url, "postgres", "8326653669a");
+            conexion = DriverManager.getConnection(url, "postgres", "esvb");
             if (conexion != null) {
                 System.out.println("Conexión a base de datos ... Ok");
             }
@@ -120,6 +120,60 @@ public class PruebaConexion {
             System.out.println("Problema para cerrar la Conexión a la base de datos ");
         }
     }
+        public Object[][] Select_Materia()
+    {
+     int registros = 0;      
+      String consulta = "Select id,nombre,dificultad,tipo FROM public.materias ";
+      String consulta2 = "Select count(*) as total from public.materias ";
+      //obtenemos la cantidad de registros existentes en la tabla
+      try{
+         PreparedStatement pstm = conexion.prepareStatement( consulta2 );
+         ResultSet res = pstm.executeQuery();
+         res.next();
+         registros = res.getInt("total");
+         res.close();
+      }catch(SQLException e){
+         System.out.println(e);
+      }
+    //se crea una matriz con tantas filas y columnas que necesite
+    Object[][] data = new String[registros][4];
+    //realizamos la consulta sql y llenamos los datos en la matriz "Object"
+      try{
+         PreparedStatement pstm = conexion.prepareStatement(consulta);
+         ResultSet res = pstm.executeQuery();
+         int i = 0;
+         while(res.next()){            
+            data[i][0] = res.getString( "id" );
+            data[i][1] = res.getString( "nombre" );
+            data[i][2] = res.getString( "dificultad" );
+            data[i][3] = res.getString( "tipo" );
+            i++;
+         }
+         res.close();
+          }catch(SQLException e){
+               System.out.println(e);
+        }
+    return data;
+    }
+
+/* Ejecuta la actualizacion de la tabla persona dado los valores de actualizacion
+ * y el ID del registro a afectar
+ */
+    public boolean update(String valores, String id)
+    {
+        boolean res = false;        
+        String q = " UPDATE public.materias SET " + valores + " WHERE id= " + id;
+        try {
+            PreparedStatement pstm = conexion.prepareStatement(q);
+            pstm.execute();
+            pstm.close();
+            res=true;
+         }catch(SQLException e){            
+            System.out.println(e);
+        }
+        return res;
+    }
+
 
     /* public static void main(String[] args) throws SQLException {
      PruebaConexion x = new PruebaConexion();
